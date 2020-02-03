1d8bb72c4a67e74e4dc53955eb9b530912953012
hatch <hatch.tech.eng@gmail.com>
2020-02-03 23:42:50 +0900

    AddTool

---
+++ EditorReplaceAssetMenu.cs
@@ -0,0 +1,34 @@
+using UnityEditor;
+using UnityEngine;
+using System.IO;
+
+namespace UniLib
+{
+	public class ReplaceAssetMenu
+	{
+		[MenuItem("Assets/Replace Selection Asset")]
+		private static void Replace()
+		{
+			var objects = Selection.objects;
+			foreach (var selectObj in objects)
+			{
+				var path = AssetDatabase.GetAssetPath(selectObj);
+
+				if (string.IsNullOrEmpty(path))
+					continue;
+
+				var fullPath = Application.dataPath.Replace("/Assets", "/" + path);
+				var ext = Path.GetExtension(path).Substring(1);
+				var fileName = Path.GetFileName(path);
+				var sourcePath = EditorUtility.OpenFilePanel("Select Replace Asset\n" + fileName, "", ext);
+
+				if (string.IsNullOrEmpty(sourcePath))
+					continue;
+
+				File.Copy(sourcePath, fullPath, true);
+			}
+		
+			AssetDatabase.Refresh();
+		}
+	}
+}
